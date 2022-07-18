c35a1cc75a31d6b264cb12b25da404c26bc5cece
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-07-18 15:49:47 -0300

    Criando views baseadas em classe

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Course
 
-# Create your views here.
+class ManageCourseListView(ListView):
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
