6af865b5ceca20b4acafbf6f4d23f5a5cf72a3b0
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-08-04 14:45:33 -0300

    Adicionando permissões às views

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from ..models import Subject, Course
 from .serializers import SubjectSerializer
 
@@ -16,6 +17,7 @@ class SubjectDetailView(generics.RetrieveAPIView):
 
 class CourseEnrollView(APIView):
     authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
