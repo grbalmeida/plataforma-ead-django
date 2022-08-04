3280a81c58e1be72eaef25e9f24ee027bb6b97c0
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-08-04 15:06:57 -0300

    Adicionando outras ações nos viewsets

--- educa/courses/api/urls.py
+++ educa/courses/api/urls.py
@@ -16,7 +16,5 @@ urlpatterns = [
         views.SubjectDetailView.as_view(),
         name='subject_detail'),
 
-    path('courses/<pk>/enroll/', views.CourseEnrollView.as_view(), name='course_enroll'),
-
     path('', include(router.urls)),
 ]
--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from ..models import Subject, Course
 from .serializers import SubjectSerializer, CourseSerializer
 
@@ -16,15 +17,15 @@ class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
-class CourseEnrollView(APIView):
-    authentication_classes = (BasicAuthentication,)
-    permission_classes = (IsAuthenticated,)
-
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
-        course.students.add(request.user)
-        return Response({'enrolled': True})
-
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    @action(detail=True,
+            methods=['post'],
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
